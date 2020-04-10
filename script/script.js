2fdb748f3c46c1c176969e82bae8741460acfa24
DUDA DEN <tayboy@ukr.net>
2020-04-10 19:03:18 +0300

    timePicker

--- script/script.js
+++ script/script.js
@@ -7,12 +7,11 @@ $(document).ready(function(){
         autoplaySpeed: 2000
     });
 
-    $(document).ready(function(){
-        $('input.timepicker').timepicker({});
-    });
+
+    $('input.timepicker').timepicker({});
 
     $('input[type="tel"]').mask('+0 (000) 00 00 000');
 
-    $('#datepicker').datepicker();
+    $('input#datepicker').datepicker();
 });
 
